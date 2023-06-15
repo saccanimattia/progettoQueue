4ef314d1c5651d36f9ca2f1ad9b1d953f7ad9f2f
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-06-15 13:52:45 +0200

    categoria

--- src/app/services/pocket-base.service.ts
+++ src/app/services/pocket-base.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import PocketBase from 'pocketbase';
-
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -8,8 +8,9 @@ import PocketBase from 'pocketbase';
 export class PocketBaseService {
   private pb: PocketBase;
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.pb = new PocketBase('');
+
   }
 
   setIp() {
@@ -36,6 +37,7 @@ export class PocketBaseService {
 
   async prendiCategorie(): Promise<any[]> {
     try {
+      console.log(this.pb)
       const result = await this.pb.collection('categoria').getList(1, 50);
       let categorie = result.items;
       return categorie
@@ -44,4 +46,29 @@ export class PocketBaseService {
     }
       return []
     }
+
+
+    downloadFile(fileUrl : any) {
+
+      this.http.get(fileUrl, { responseType: 'blob' }).subscribe((response: Blob) => {
+        const blob = new Blob([response], { type: response.type });
+
+        // Crea un URL oggetto per il blob
+        const url = window.URL.createObjectURL(blob);
+
+        // Crea un link temporaneo per il download del file
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'file.pdf'; // Specifica il nome del file da scaricare
+        link.click();
+
+        // Rilascia la risorsa URL oggetto
+        window.URL.revokeObjectURL(url);
+      });
+    }
+
+
+
+
+
 }
