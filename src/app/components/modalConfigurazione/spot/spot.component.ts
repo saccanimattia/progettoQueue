a4fd98d6252475ffcc05b375567aa799b08d38fd
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-06-15 17:16:53 +0200

    spot

--- src/app/components/modalConfigurazione/spot/spot.component.ts
+++ src/app/components/modalConfigurazione/spot/spot.component.ts
@@ -9,130 +9,121 @@ import { PocketBaseService } from 'src/app/services/pocket-base.service';
 })
 export class SpotComponent {
   @Output() buttonClick = new EventEmitter<void>();
-  disabilitato = true
+  disabilitato = true;
 
+  selectedSpot: any[] = [];
+  items: any[] = [];
 
-  selectedSpot : any[] = []
-  items : any[] = []
-  constructor(private pocketBase : PocketBaseService, private salvadati : DatiDispositivoService){
+  constructor(private pocketBase: PocketBaseService, private salvadati: DatiDispositivoService) {}
 
-  }
-
-  ngOnInit(){
-    this.prendiSpot()
+  ngOnInit() {
+    this.prendiSpot();
   }
 
   async openModal() {
+    console.log("apri modsl")
     const modal = document.querySelector('#categorieModal');
     modal?.classList.add('show');
     modal?.setAttribute('style', 'display: block');
-    console.log(this.items)
+    console.log(this.items);
   }
 
   async closeModal() {
     const modal = document.querySelector('#categorieModal');
-
-      modal?.classList.remove('show');
-      modal?.setAttribute('style', 'display: none');
-      this.buttonClick.emit();
-
+    modal?.classList.remove('show');
+    modal?.setAttribute('style', 'display: none');
+    this.buttonClick.emit();
   }
 
   async prendiSpot(): Promise<void> {
-    this.pocketBase.setIp()
-    let a:any = await this.pocketBase.prendiSpot()
+    await this.pocketBase.setIp();
+    let a: any = await this.pocketBase.prendiSpot();
     this.items = a;
-    console.log(this.items)
-    this.scaricaImmagini()
-}
-
-async scaricaImmagini() {
-  for (let i = 0; i < this.items.length; i++) {
-    const item = this.items[i];
-    let newMedias = [];
-    for (let j = 0; j < item.medias.length; j++) {
-      const m = item.medias[j];
-      const x = await this.pocketBase.prendiRisorsa(m);
-      const id = m;
-      const imageUrl = localStorage.getItem('indirizzoIp') + "/api/files/" + x.collectionId + '/' + x.id + '/' + x.file + '?thumb=100x100&token=';
-      console.log("link immagine");
-      console.log(imageUrl);
-      newMedias.push(imageUrl);
-    }
-    for(let ii = 0; ii<newMedias.length; ii++){
-      console.log(item.medias[ii].id)
-      item.medias[ii].id == newMedias[ii]
-    }
-    console.log(newMedias)
-    console.log(item.medias)
+    console.log(this.items);
+    this.scaricaImmagini();
   }
-  console.log("junfditr53ugbu5iy");
-  console.log(this.items);
-  this.openModal();
-}
 
-  modificaStato(item : any, i : number){
-    console.log(item)
-    console.log(i)
-    const elem = document.querySelector('#elem' + i);
-    if(this.selectedSpot.length == 0 ){
-      console.log("0 elem")
-      this.selectedSpot.push(item)
-      elem?.classList.add('sel');
+  async scaricaImmagini() {
+    for (let i = 0; i < this.items.length; i++) {
+      const item = this.items[i];
+      let newMedias = [];
+      for (let j = 0; j < item.medias.length; j++) {
+        const m = item.medias[j];
+        const x = await this.pocketBase.prendiRisorsa(m);
+        const id = m;
+        const imageUrl = localStorage.getItem('indirizzoIp') + "/api/files/" + x.collectionId + '/' + x.id + '/' + x.file + '?thumb=100x100&token=';
+        console.log("link immagine");
+        console.log(imageUrl);
+        newMedias.push(imageUrl);
+      }
+      item.medias = newMedias;
     }
-    else if(this.isPresente(item)){
-      console.log("elem presente")
+    console.log("junfditr53ugbu5iy");
+    console.log(this.items);
+    this.openModal();
+  }
+
+  modificaStato(item: any, i: number) {
+    console.log(item);
+    console.log(i);
+    const elem = document.querySelector('#elem' + i);
+    if (this.selectedSpot.length == 0) {
+      console.log("0 elem");
+      this.selectedSpot.push(item);
+      elem?.classList.add('sel');
+    } else if (this.isPresente(item)) {
+      console.log("elem presente");
       const index = this.selectedSpot.indexOf(item);
       if (index !== -1) {
         this.selectedSpot.splice(index, 1);
       }
       elem?.classList.remove('sel');
-    }
-    else{
-      console.log("elem non presente")
-      this.selectedSpot.push(item)
+    } else {
+      console.log("elem non presente");
+      this.selectedSpot.push(item);
       elem?.classList.add('sel');
     }
-    console.log(this.selectedSpot)
-    if(this.selectedSpot.length == 0){
+    console.log(this.selectedSpot);
+    if (this.selectedSpot.length == 0) {
       this.disabilitato = true;
     }
-    if(this.selectedSpot.length > 0){
+    if (this.selectedSpot.length > 0) {
       this.disabilitato = false;
     }
   }
 
-  isPresente(item : any){
-    for(let i of this.selectedSpot){
-      if(i === item)
-        return true
+  isPresente(item: any) {
+    for (let i of this.selectedSpot) {
+      if (i === item)
+        return true;
     }
-    return false
+    return false;
   }
 
-  save(){
+  save() {
     this.salvadati.setCategories(this.selectedSpot);
   }
 
   isImage(media: string): boolean {
     // Ottieni l'estensione del file
-    const extension = media.split('.').pop()?.toLowerCase();
+    let extension = media.substring(media.length-24, media.length-21).toLowerCase();
+    console.log(extension);
 
     // Array di estensioni di file immagine supportate
     const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
 
     // Verifica se l'estensione del file corrisponde a un'immagine supportata
-    return imageExtensions.includes(extension!);
+    return imageExtensions.includes(extension);
   }
 
   isVideo(media: string): boolean {
     // Ottieni l'estensione del file
-    const extension = media.split('.').pop()?.toLowerCase();
+    const extension = media.substring(media.length-24, media.length-21).toLowerCase();
 
     // Array di estensioni di file video supportate
     const videoExtensions = ['mp4', 'avi', 'mov', 'mkv'];
 
     // Verifica se l'estensione del file corrisponde a un video supportato
-    return videoExtensions.includes(extension!);
+    return videoExtensions.includes(extension);
   }
 }
