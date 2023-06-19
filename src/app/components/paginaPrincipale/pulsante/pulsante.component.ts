d50fade0ad73ffdda75fffa76a277f7fbd83fe14
saccanimattia <mattia.saccani@studenti.iispascal.it>
2023-06-19 08:25:42 +0200

    wwww

--- src/app/components/paginaPrincipale/pulsante/pulsante.component.ts
+++ src/app/components/paginaPrincipale/pulsante/pulsante.component.ts
@@ -1,40 +1,50 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { PocketBaseService } from 'src/app/services/pocket-base.service';
-
 
 @Component({
   selector: 'app-pulsante',
   templateUrl: './pulsante.component.html',
   styleUrls: ['./pulsante.component.scss']
 })
-export class PulsanteComponent {
+export class PulsanteComponent implements OnInit {
   @Input() gruppo: any;
-  group :  any = undefined
-  x : any
-  img : any
+  group: any = undefined;
+  x: any;
+  img: any;
+  timeoutLimit = 5000; // Tempo limite in millisecondi (es. 5000 = 5 secondi)
 
+  constructor(private pocketBase: PocketBaseService) {}
 
-  constructor(private pocketBase : PocketBaseService){
-  }
-
-
-  async ngOnInit(): Promise<void> {
+  ngOnInit(): void {
     console.log(this.gruppo);
     console.log("i35tr");
-    this.group = await this.pocketBase.prendiCategoriaId(this.gruppo);;
-    console.log("ir")
-    console.log(this.group.image);
-    this.x = await this.pocketBase.prendiRisorsa(this.group.image)
-    console.log(this.x)
-    this.img = localStorage.getItem('indirizzoIp') + "/api/files/" + this.x.collectionId + '/' + this.x.id + '/' + this.x.file + '?thumb=100x100&token=';
+    this.group = this.gruppo;
+
+    const timeoutPromise = new Promise((resolve, reject) => {
+      setTimeout(() => reject('Tempo limite superato'), this.timeoutLimit);
+    });
+
+    Promise.race([this.pocketBase.prendiCategoriaId(this.gruppo), timeoutPromise])
+      .then((groupData: any) => {
+        console.log("ir");
+        this.group = groupData;
+        console.log(this.group.image);
+        return this.pocketBase.prendiRisorsa(this.group.image);
+      })
+      .then((xData: any) => {
+        console.log(xData);
+        this.x = xData;
+        this.img = localStorage.getItem('indirizzoIp') + "/api/files/" + this.x.collectionId + '/' + this.x.id + '/' + this.x.file + '?thumb=100x100&token=';
+      })
+      .catch((error) => {
+        console.error('Errore durante il recupero delle informazioni:', error);
+        // Esegui le azioni di gestione dell'errore qui, ad esempio mostrare un messaggio all'utente
+      });
   }
 
-
-
-
-  add(){
-    this.group.number = this.group.number + 1
-    this.group.queued = this.group.queued + 1
-    this.pocketBase.updateGroup(this.group.id, this.group)
+  add(): void {
+    this.group.number = this.group.number + 1;
+    this.group.queued = this.group.queued + 1;
+    this.pocketBase.updateGroup(this.group.id, this.group);
   }
 }
