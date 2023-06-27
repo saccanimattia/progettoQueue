e0f33ddaef902fe673a7bc1979aac88b81f30f22
simonepanfilii <simone.panfili@studenti.iispascal.it>
2023-06-27 08:56:33 +0200

    dddddd

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -24,4 +24,5 @@ export class AppComponent {
       this.router.navigate(['/paginaPrincipale']);
     }
   }
+
 }
